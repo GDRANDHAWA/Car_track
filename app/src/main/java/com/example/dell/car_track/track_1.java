ee2765954e51c64cff4fcd65dff6e499027f7274
gd randhawa <randhawagurmandeep@gmail.com>
2017-02-05 22:52:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/car_track/track_1.java
@@ -0,0 +1,21 @@
+package com.example.dell.car_track;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 2/5/2017.
+ */
+
+public class track_1 extends AppCompatActivity {
+@Override
+protected void onCreate( Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.track_1);
+}
+
+
+
+
+}
---
+++ app/src/main/java/com/example/dell/car_track/track_2.java
@@ -0,0 +1,17 @@
+package com.example.dell.car_track;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 2/5/2017.
+ */
+
+public class track_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.track_2);
+    }
+}
